efeedaa7d511fe459f907d4bb6b2f50de37354fb
YardenW91 <43200769+YardenW91@users.noreply.github.com>
2018-09-12 12:19:07 +0300

    Update createMap.py
    added comments

--- createMap.py
+++ createMap.py
@@ -5,7 +5,9 @@ import xml.etree.ElementTree as ET
 
 testimony_file = sys.argv[1]
 about_file = sys.argv[2]
+# map of key: location name and info (sentences and coordinates)
 locations = {}
+# names of locations only
 location_names = []
 tree = ET.parse(testimony_file)
 root = tree.getroot()
@@ -17,6 +19,7 @@ with open(testimony_file, 'r') as myfile:
     data = myfile.read().replace('\n', '')
 
 
+# returns location name if in location_names, -1 otherwise
 def find_location(myLocation):
     for loc in location_names:
         if loc.find(myLocation) != -1:
@@ -24,6 +27,7 @@ def find_location(myLocation):
     return -1
 
 
+# extract text only from testimony
 body_txt = re.match('(.*)<body>(.*)</body>(.*)', data).groups()[1]
 body_txt = re.sub('</l>', "\n", body_txt)
 body_txt = re.sub('<.*?>', '', body_txt)
@@ -37,6 +41,7 @@ i = 0
 for parent in root.iter():
     for child in parent:
         if child.tag.find('placeName') != -1:
+            # first time location is mentioned
             if find_location(child.text) == -1:
                 location_names.append(child.text)
                 locations[child.text] = {}
@@ -47,8 +52,10 @@ for parent in root.iter():
                 taggedLine = parent.text
                 for c in parent:
                     i += 1
+                    # line for info-window
                     line = line + "<a href=\"#A"+str(i)+"\">" + c.text + "</a>" + c.tail
                     realLine = realLine + c.text + c.tail
+                    # line for testimony text
                     taggedLine = taggedLine + "<h id=\"A"+str(i)+"\">" + c.text + "</h>" + c.tail
                 if line.find("Interviewer:") == -1:
                     if realLine not in listOfRealLines:
@@ -60,16 +67,20 @@ for parent in root.iter():
                     body_txt = body_txt.replace(realLine, taggedLine)
 
                 else:
+                    # no need to add if this is a line the interviewer said
                     locations[child.text]["lines"] = ""
 
             else:
+                # not first time location is mentioned
                 line = parent.text
                 taggedLine = parent.text
                 realLine = parent.text
                 for c in parent:
                     i += 1
+                    # line for info-window
                     line = line + "<a href=\"#A"+str(i)+"\">" + c.text + "</a>" + c.tail
                     realLine = realLine + c.text + c.tail
+                    # line for testimony text
                     taggedLine = taggedLine + "<h id=\"A"+str(i)+"\">" + c.text + "</h>" + c.tail
                 if line.find("Interviewer:") == -1:
                     if realLine not in listOfRealLines:
@@ -78,12 +89,15 @@ for parent in root.iter():
                         locations[find_location(child.text)]["lines"] = locations[find_location(child.text)][
                                                                             "lines"] + line + "<br /><br />"
                     else:
+                        # lina has already appeared in a mention of a location earlier. pull the tagged line from before
+                        # with the tags that were already given
                         if locations[find_location(child.text)]["lines"].find(listOfLines[listOfRealLines.index(realLine)]) == -1:
                             locations[find_location(child.text)]["lines"] = locations[find_location(child.text)][
                                                                             "lines"] + listOfLines[listOfRealLines.index(realLine)] + "<br /><br />"
                     body_txt = body_txt.replace(realLine, taggedLine)
 
 
+# create an HTML file, a website with two tabs and a google map with info-windows
 file = codecs.open("Testimony documentation.html", 'w')
 
 file.write("<!DOCTYPE html>\n"
@@ -140,7 +154,7 @@ file.write("<h2 style=\"font-family:verdana;\"><center>Map of Family Journey</ce
            "    zoom:3,\n"
            "};\n"
            "var map=new google.maps.Map(document.getElementById(\"googleMap\"),mapProp);\n")
-
+# add all locations to map, with info-window
 for j in range(0, len(location_names)):
     i = str(j)
     file.write("var myPoint"+i+" = new google.maps.LatLng("+locations[location_names[j]]["lon"]+","+locations[location_names[j]]["lat"]+");\n"
@@ -152,14 +166,14 @@ for j in range(0, len(location_names)):
     file.write("    });\n")
     file.write("infowindow"+i+".open(map, marker"+i+");\n")
     file.write("});")
-
+# insert API key here!
 file.write("}\n"
            "</script>\n\n"
            "<script src="
            "\"https://maps.googleapis.com/maps/api/js?key=INSERT-GOOGLE-API-KEY&callback=myMap\">\n"
            "</script>\n\n")
 
-
+# add testimony and about text to tabs
 file.write("<br /><h2>Testimony</h2>\n"
            "<br />"+body_txt+"</p>\n"
            "</div>\n\n"
